c07cb84197973c49f2d9c7d5e19b30ea4fdcbb4a
shure22 <shuo@Shawns-Mac-Pro.local>
2018-02-11 19:53:05 -0800

    Clean up files structure, add start screen and scroll view

---
+++ Assets/Scripts/GameScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScreen : MonoBehaviour {
+    public static GameObject Screen;
+
+    void Awake()
+    {
+        Screen = gameObject;
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScreen : MonoBehaviour {
+    public static GameObject Screen;
+
+    void Awake()
+    {
+        Screen = gameObject;
+    }
+
+	// Use this for initialization
+	void Start () {
+        GameScreen.Screen.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ButtonOnClick() {
+        Screen.SetActive(false);
+        GameScreen.Screen.SetActive(true);
+    }
+}
